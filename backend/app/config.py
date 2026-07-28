c6c5fa7f1990f2291803397d8184230ed1c278db
Kamal Ibrahim <KamalIbrahim@my.uopeople.edu>
2026-07-28 14:40:27 -0700

    Enable SSL for Azure MySQL

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,17 +8,13 @@ class Config:
 
    SQLALCHEMY_DATABASE_URI = (
     f"mysql+pymysql://"
-    f"{os.getenv('DB_USER')}:"
-    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
     f"{os.getenv('DB_HOST')}:"
     f"{os.getenv('DB_PORT', '3306')}/"
     f"{os.getenv('DB_NAME')}"
+    "?ssl_verify_cert=false"
 )
 
-SQLALCHEMY_ENGINE_OPTIONS = {
-    "connect_args": {
-        "ssl": {}
-    }
-}
+
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
